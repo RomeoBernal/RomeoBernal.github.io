c5f09fab5e69533ce4a0593d8c53e5583196f8b1
RomeoBernal <romeo.bernal@outlook.com>
2026-06-23 20:25:13 +0200

    Un par de cambios

--- js/main.js
+++ js/main.js
@@ -75,7 +75,24 @@
     sections.forEach(s => io.observe(s));
   }
 
+  function initCopyButtons() {
+    document.querySelectorAll('.btn-copy').forEach(btn => {
+      btn.addEventListener('click', () => {
+        navigator.clipboard.writeText(btn.dataset.copy).then(() => {
+          btn.classList.add('copied');
+          const icon = btn.querySelector('svg');
+          icon.innerHTML = '<polyline points="20 6 9 17 4 12"/>';
+          setTimeout(() => {
+            btn.classList.remove('copied');
+            icon.innerHTML = '<rect x="9" y="9" width="13" height="13" rx="2"/><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>';
+          }, 2000);
+        });
+      });
+    });
+  }
+
   renderFilters();
   renderProjects('Todos');
   initActiveNav();
+  initCopyButtons();
 })();
